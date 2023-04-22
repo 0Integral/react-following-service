5a826c6bc11d5eb55abb8660089c2f46db26bab6
unknown <vlad.vtorygin@gmail.com>
2023-04-22 14:20:09 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,28 +5,28 @@ import { TweetsList } from './App.styled';
 
 export const App = () => {
   const [userStatistics, setUserStatistic] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const getFunc = async () => {
-    setLoading(true);
-    try {
-      const data = await getUsersBase();
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const getFunc = async () => {
+  //   setLoading(true);
+  //   try {
+  //     const data = await getUsersBase();
 
-      setUserStatistic(data);
-    } catch (er) {
-      setError(er);
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  //     setUserStatistic(data);
+  //   } catch (er) {
+  //     setError(er);
+  //     console.log(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
   useEffect(() => {
-    getFunc();
+    getUsersBase().then(results => setUserStatistic([...results]));
   }, []);
 
   return (
     <div>
-      {loading && <div>Await plz</div>}
+      {/* {loading && <div>Await plz</div>} */}
       <TweetsList>
         {userStatistics.map(user => {
           return (
